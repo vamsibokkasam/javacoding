efb170a5ee11546675c3721f23a388ef94c4d64d
BOKKASAM vamsi <vamsib170@gmail.com>
2025-01-25 06:00:59 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("welcome "+name);
+    }
+}
