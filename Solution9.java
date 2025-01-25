b0f9d81c06b3b05987c4c42212105dace183eb61
BOKKASAM vamsi <vamsib170@gmail.com>
2025-01-25 06:02:21 +0530

    Add files via upload

---
+++ Solution9.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n%10+(n/10)%10+(n/100)%10);
+    }
+}
